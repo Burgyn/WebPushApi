76756f16232aa2284f62374532ff47de62f97c88
Matej Martiniak <matej.martiniak@kros.sk>
2024-01-16 22:21:14 +0100

    added create and get

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+using System.Text.Json;
+using WebPush;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+const string fileName = "subscriptions.json";
+app.MapGet("/subscriptions", async () => await PushSubscriptions(fileName));
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/subscriptions", async (PushSubscription subscription) =>
+{
+   var subscriptions = await PushSubscriptions(fileName);
+   subscriptions.Add(subscription);
+   
+   File.WriteAllText(fileName,JsonSerializer.Serialize(subscriptions));
+});
 
 app.Run();
+
+async Task<List<PushSubscription>> PushSubscriptions(string filename)
+{
+   if (!File.Exists(fileName))
+   {
+      return [];
+   }
+   List<PushSubscription> pushSubscriptions = [];
+   string data = await File.ReadAllTextAsync(filename);
+   if (!string.IsNullOrEmpty(data))
+   {
+      pushSubscriptions = JsonSerializer.Deserialize<List<PushSubscription>>(data) ?? [];
+   }
+
+   return pushSubscriptions;
+}
